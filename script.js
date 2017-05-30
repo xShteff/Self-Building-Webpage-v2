22df624164b214c27dbe74c1c00939930e287fd6
Alin-Stefan Olaru <allenmcpotter@outlook.com>
2017-05-30 03:01:18 +0200

    Couldn't sleep... had to find something to do.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var SelfBuildingSite = {
     Settings: {
         intervalTime: 50,
-        breakTime: 1000,
+        breakTime: 500,
         textInputSelector: '#textInput',
         tabSpace: 3
     },
@@ -9,6 +9,28 @@ var SelfBuildingSite = {
         newLine: /(\r\n|\n|\r)/gm,
         endSentence: /(\.|\!|\?)/gm,
         tab: /(\t)/gm,
+        commentStart: /(\/\*)/gm,
+        commentEnd: /(\*\/)/gm,
+        isCommentStart: function (c) {
+            var chars = [
+                SelfBuildingSite.Content.text.charAt(SelfBuildingSite.Content.currentCharCount),
+                SelfBuildingSite.Content.text.charAt(SelfBuildingSite.Content.currentCharCount + 1),
+            ]
+            if (chars[0] === "/" && chars[1] == "*")
+                return true;
+
+            return false;
+        },
+        isCommentEnd: function (c) {
+            var chars = [
+                SelfBuildingSite.Content.text.charAt(SelfBuildingSite.Content.currentCharCount),
+                SelfBuildingSite.Content.text.charAt(SelfBuildingSite.Content.currentCharCount + 1),
+            ]
+            if (chars[0] === "*" && chars[1] == "/")
+                return true;
+
+            return false;
+        },
         isTab: function (c) {
             var result = true;
             var chars = [
@@ -32,8 +54,33 @@ var SelfBuildingSite = {
     Content: {
         currentCharCount: 0,
         text: $('#cheatsydoodle').text(),
+        stripHTML: function (text) {
+            return text.replace(/<(?:[^>=]|='[^']*'|="[^"]*"|=[^'"][^\s>]*)*>/gm, "");
+        },
+        stripHTMLbutBr: function (text) {
+            return text.replace(/<(?!br\s*\/?)[^>]+>/gm, "");
+        },
+        fancifyEverything: function () {
+            var old = $('#textInput').html();
+            $('#textInput').html(SelfBuildingSite.Content.stripHTMLbutBr(old)
+                .replace(/\/\*/gm, "<span class='comment'>/*")
+                .replace(/\*\//gm, "*/</span>")
+                .replace(/<br>/gm, "\n")
+                .replace(/(\.|#)(.*)(\d|){/gm, "<span class='selectorTwo'>$1$2</span> {")
+                .replace(/(body)(.*)(\d|){/gm, "<span class='selectorOne'>$1</span> {") //Meh, will find another way of doing it.
+                .replace(/(.*):(.*);/gm, "<span class='property'>$1</span>:<span class='value'>$2</span>;")
+                .replace(/\n/gm, "<br>")
+            );
+        },
         isDone: function () {
             return SelfBuildingSite.Content.currentCharCount === SelfBuildingSite.Content.text.length;
+        },
+        startSpan: function (className) {
+            var spanStart = "<span class='" + className + "'>";
+            $(SelfBuildingSite.Settings.textInputSelector).append(spanStart);
+        },
+        endSpan: function () {
+            $(SelfBuildingSite.Settings.textInputSelector).append("</span>");
         },
         addTab: function () {
             for (var i = 0; i < SelfBuildingSite.Settings.tabSpace; i++)
@@ -47,13 +94,16 @@ var SelfBuildingSite = {
             $(SelfBuildingSite.Settings.textInputSelector).html(displayText + c);
         },
         UpdateStyleSheet: function () {
-            $('#stylesheet').text($(SelfBuildingSite.Settings.textInputSelector).text().replace(/\s\s\s/g, ' '));
+            $('#stylesheet').text(SelfBuildingSite.Content.stripHTML($(SelfBuildingSite.Settings.textInputSelector).text().replace(/\s\s\s/g, ' ')));
         }
     },
     Progress: function () {
         var character = SelfBuildingSite.Content.text.charAt(SelfBuildingSite.Content.currentCharCount);
         var isEndSentence = SelfBuildingSite.Regex.isEndSentence(character); //For some reason I have to do it like this.
         var isTab = SelfBuildingSite.Regex.isTab();
+        var isCmntStart = SelfBuildingSite.Regex.isCommentStart();
+        var isCmntEnd = SelfBuildingSite.Regex.isCommentEnd();
+
         if (SelfBuildingSite.Regex.isNewLine(character))
             SelfBuildingSite.Content.addNewLine();
         else if (isTab)
@@ -67,6 +117,8 @@ var SelfBuildingSite = {
             else
                 setTimeout(SelfBuildingSite.Progress, SelfBuildingSite.Settings.intervalTime);
 
+        $('#textInput').scrollTop($('#textInput')[0].scrollHeight);
+        SelfBuildingSite.Content.fancifyEverything();
         SelfBuildingSite.Content.UpdateStyleSheet();
         if (isTab)
             SelfBuildingSite.Content.currentCharCount += 2;
